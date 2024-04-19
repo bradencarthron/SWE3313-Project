da2a8eebee78f7f2e7708daf42aca6824dda905c
bradencarthron <80701392+bradencarthron@users.noreply.github.com>
2024-04-19 17:32:55 -0400

    test again

--- demo/src/main/java/com/example/Main.java
+++ demo/src/main/java/com/example/Main.java
@@ -33,8 +33,8 @@ public class Main extends Application {
 
     public static void main(String[] args) {
         launch();
-        System.out.println("Hello From Bradem");
-        
+        System.out.println("Hello From Braden");
+
     }
 
 }
